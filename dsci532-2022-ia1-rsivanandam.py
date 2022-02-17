f3d7ac0b577a80a5e49450a6ae1c5ab61d56b13d
Rowan Sivanandam <rowan.sivanandam@gmail.com>
2022-02-17 12:07:53 -0800

    fixes to run heroku

--- dsci532-2022-ia1-rsivanandam.py
+++ dsci532-2022-ia1-rsivanandam.py
@@ -2,18 +2,11 @@ from dash import Dash, html, dcc, Input, Output
 import altair as alt
 import pandas as pd
 
-iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-
-def plot_altair(xcol):
-    chart = alt.Chart(iris).mark_point().encode(
-        x=xcol,
-        y='petal_length',
-        tooltip='sepal_length').interactive()
-    return chart.to_html()
-
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 server = app.server
+
+iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
 app.layout = html.Div([
         dcc.Dropdown(
@@ -21,14 +14,20 @@ app.layout = html.Div([
             options=[{'label': i, 'value': i} for i in iris.columns]),
         html.Iframe(
             id='scatter',
-            style={'border-width': '0', 'width': '100%', 'height': '400px'},
-            srcDoc=plot_altair(xcol='sepal_length'))])
+            style={'border-width': '0', 'width': '100%', 'height': '400px'})])
 
 @app.callback(
     Output('scatter', 'srcDoc'),
     Input('xcol', 'value'))
 def update_output(xcol):
     return plot_altair(xcol)
+
+def plot_altair(xcol):
+    chart = alt.Chart(iris).mark_point().encode(
+        x=xcol,
+        y='petal_length',
+        tooltip='sepal_length').interactive()
+    return chart.to_html()
         
 if __name__ == '__main__':
     app.run_server(debug=True)
